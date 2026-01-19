55effa8bf092b4eb77facce46d633f77d702799c
PetkoPopov <poetpoet2014@mail.ru>
2026-01-19 19:51:54 +0200

    some html and second step

--- defi.js
+++ defi.js
@@ -25,19 +25,30 @@ button.addEventListener("click", () => {
     let angel_number = document.getElementById("angel_number");
     let boris_number = document.getElementById("boris_number");
     let mode_number = document.getElementById("mode_number");
-    let result = document.getElementById("result");
+    let res_angel = document.getElementById("result_angel");
+    let res_boris = document.getElementById("result_boris");
 let scr1= numExpMod(is_simple,
     Number(key_number.value),
     Number(angel_number.value),
     Number(mode_number.value)
    );
-   result.innerText = scr1 ;
+
+
+   res_angel.innerHTML = "Ангел извършва операцията " +key_number.value + " ^ " + angel_number.value + " mod " + mode_number.value + ' = <h2>'+scr1+'</h2>' ;
    let scr2= numExpMod(is_simple,
     Number(key_number.value),
     Number(boris_number.value),
     Number(mode_number.value)
    );
-setTimeout(()=>{
-    result.innerText =  scr2;
-},1000) 
+    res_boris.innerHTML = 'Борис извършва операцията ' +key_number.value + " ^ " + boris_number.value + " mod " + mode_number.value + ' = <h2>'+scr2+'</h2>' ; 
+   document.getElementById("second_step").style.display = "block";
+    let btn_second = document.getElementById("btn_second_step");
+      btn_second.style.backgroundColor = "lightblue";
+        btn_second.style.padding = "10px";
+        btn_second.style.borderRadius = "7px";
+        btn_second.style.fontSize = "20px";
+        btn_second.style.fontWeight = "bold";   
+        btn_second.style.width ="fit-content";      
+        let result_div = document.getElementById("final_result");
+        result_div.style.display = "block";
 })
