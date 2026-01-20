d45684b906f8cd9fe0aebfbca5e0c5db747979d6
PetkoPopov <poetpoet2014@mail.ru>
2026-01-20 17:40:17 +0200

    some explenation finaly

--- defi.js
+++ defi.js
@@ -43,7 +43,9 @@ let scr1= numExpMod(is_simple,
     res_boris.innerHTML = 'Борис извършва операцията ' +key_number.value + " ^ " + boris_number.value + " mod " + mode_number.value + ' = <h2>'+scr2+'</h2>' ; 
    document.getElementById("second_step").style.display = "block";
     let btn_second = document.getElementById("btn_second_step");
-      btn_second.style.backgroundColor = "lightblue";
+    let result_angel=0; 
+    let result_boris=0 ;
+    btn_second.style.backgroundColor = "lightblue";
         btn_second.style.padding = "10px";
         btn_second.style.borderRadius = "7px";
         btn_second.style.fontSize = "20px";
@@ -51,4 +53,23 @@ let scr1= numExpMod(is_simple,
         btn_second.style.width ="fit-content";      
         let result_div = document.getElementById("final_result");
         result_div.style.display = "block";
+        btn_second.addEventListener("click", () => {
+        result_angel = numExpMod(is_simple,scr1,Number(boris_number.value),Number(mode_number.value));
+        result_boris = numExpMod(is_simple,scr2,Number(angel_number.value),Number(mode_number.value));
+        let expl = document.getElementById("explanation");
+        // expl.style.display="flex";
+        // expl.style.flexFlow="column";
+        // expl.style.marginTop="20px";
+        // expl.style.padding="10px";
+        // expl.style.border="2px solid black";
+        // expl.style.borderRadius="7px";
+
+        let expl_angel= document.querySelectorAll("#explanation div")[0];
+        let expl_boris= document.querySelectorAll("#explanation div")[1];
+        // expl.style.display="block";
+        expl_angel.innerHTML="Ангел извършва операцията " + scr2 + " ^ " + angel_number.value + " mod " + mode_number.value + ' = <h2>'+result_angel+'</h2>' ;
+        expl_boris.innerHTML="Борис извършва операцията " + scr1 + " ^ " + boris_number.value + " mod " + mode_number.value + ' = <h2>'+result_boris+'</h2>' ;
+        
+
+    })
 })
