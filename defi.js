0780ddef0f651bc189e08ec98fa6bcf900d965ed
PetkoPopov <poetpoet2014@mail.ru>
2026-01-20 19:14:58 +0200

    explenations

--- defi.js
+++ defi.js
@@ -41,35 +41,58 @@ let scr1= numExpMod(is_simple,
     Number(mode_number.value)
    );
     res_boris.innerHTML = 'Борис извършва операцията ' +key_number.value + " ^ " + boris_number.value + " mod " + mode_number.value + ' = <h2>'+scr2+'</h2>' ; 
-   document.getElementById("second_step").style.display = "block";
-    let btn_second = document.getElementById("btn_second_step");
+    
+        let result_div = document.getElementById("final_result");
+        result_div.innerHTML = 
+        "Сега Ангел и Борис разменят своите резултати.Ако злонамерен потребител прихване изчисленото от Ангел <h2>" +scr1+"</h2>  например ,той не може в разумен срок да разбере тайното число на Ангел.За целта той трябва да тества публичното число key_num^2 mod=?, key_num^3mod=?, key_num^4mod=?, key_num^5mod=? и така много пъти докато стигне до Mode-1 ! това ще отнеме много време дори със супер компютър";
+ 
+ 
+    /////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////
+    /////////////                            ////////////////////////////////////////////
+    /////////////                            ///////////////////////////////////////////
+    /////////////                            ///////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////
+    let next_step = document.getElementById("result");
+    next_step.style.display = "flex";
+    next_step.style.flex = "row";
+    document.getElementById("next_step").style.display = "block";
+    
+    let btn_second = document.getElementById("btn_next_step");
     let result_angel=0; 
     let result_boris=0 ;
     btn_second.style.backgroundColor = "lightblue";
+
         btn_second.style.padding = "10px";
         btn_second.style.borderRadius = "7px";
         btn_second.style.fontSize = "20px";
         btn_second.style.fontWeight = "bold";   
         btn_second.style.width ="fit-content";      
-        let result_div = document.getElementById("final_result");
-        result_div.style.display = "block";
-        btn_second.addEventListener("click", () => {
-        result_angel = numExpMod(is_simple,scr1,Number(boris_number.value),Number(mode_number.value));
-        result_boris = numExpMod(is_simple,scr2,Number(angel_number.value),Number(mode_number.value));
+
+
+
+
         let expl = document.getElementById("explanation");
-        // expl.style.display="flex";
-        // expl.style.flexFlow="column";
-        // expl.style.marginTop="20px";
-        // expl.style.padding="10px";
-        // expl.style.border="2px solid black";
-        // expl.style.borderRadius="7px";
+        expl.style.display="flex";
+        expl.style.flex ="row";
+        
+        let expl_div = document.createElement("div");
+        
+        btn_second.addEventListener("click", () => {
+            result_angel = numExpMod(is_simple,scr1,Number(boris_number.value),Number(mode_number.value));
+            result_boris = numExpMod(is_simple,scr2,Number(angel_number.value),Number(mode_number.value));
+            
+            expl_div.innerHTML = "Сега и Ангел и Борис разполагат с едно и също число <h2>"+result_angel+"</h2> с което да шифрират (на английски криптират) своите съобщения "
+            expl.appendChild(expl_div);
 
         let expl_angel= document.querySelectorAll("#explanation div")[0];
         let expl_boris= document.querySelectorAll("#explanation div")[1];
-        // expl.style.display="block";
-        expl_angel.innerHTML="Ангел извършва операцията " + scr2 + " ^ " + angel_number.value + " mod " + mode_number.value + ' = <h2>'+result_angel+'</h2>' ;
-        expl_boris.innerHTML="Борис извършва операцията " + scr1 + " ^ " + boris_number.value + " mod " + mode_number.value + ' = <h2>'+result_boris+'</h2>' ;
-        
+        let tmp_expl_angel = expl_angel.textContent;
+        let tmp_expl_boris = expl_boris.textContent;
+        expl_angel.innerHTML=tmp_expl_angel+ "<br>Ангел извършва операцията " + scr2 + " ^ " + angel_number.value + " mod " + mode_number.value + ' = <h2>'+result_angel+'</h2></br>' ;
+        expl_boris.innerHTML=tmp_expl_boris+"<br>Борис извършва операцията " + scr1 + " ^ " + boris_number.value + " mod " + mode_number.value + ' = <h2>'+result_boris+'</h2></br>' ;
+        btn_second.style.display = "none"; 
 
     })
 })
